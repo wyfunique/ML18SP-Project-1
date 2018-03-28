f2450ed79ffab1beed2cb53c55084ddd16569ee3
wyfunique <yifanwang2017@gmail.com>
2018-03-28 15:22:03 -0400

    Changes to be committed: 	modified:   projectYF.py

--- projectYF.py
+++ projectYF.py
@@ -4,6 +4,8 @@ from sklearn.mixture import GaussianMixture as GMM
 from skimage import morphology
 from skimage.filters import rank, gaussian
 from skimage.color.adapt_rgb import adapt_rgb, each_channel, hsv_value
+from skimage.transform import rescale
+from sklearn.cluster import SpectralClustering
 
 kernel4Orig = morphology.square(width=25)
 
@@ -38,4 +40,32 @@ def MyGMM(image, numClust, filterType=None): # Only support RGB now.
     clustIm = np.reshape(clustLabels, (height, width))
     return clustIm
 
-
+def MySpectral(image, numClust, scaleFactor=0.3, affinity='nearest_neighbors', n_neighbors=20, n_jobs=-1):
+    """
+    if filterType == 'mean':
+        pass
+    if filterType == 'median':
+        pass
+    """    
+    #if numClust > 30:
+    #    scaleFactor = scaleFactor / 1.5
+    print 'Rescaling image to %f...'% (scaleFactor**2)
+    image = np.uint8(rescale(image, scaleFactor, preserve_range=True))
+    print 'New shape: %s'%str(image.shape)
+    
+    height = image.shape[0]
+    width = image.shape[1]
+    n_features = image.shape[2]
+    n_samples = height * width
+    print 'Reshaping image...'
+    data = np.reshape(image, (n_samples, n_features))
+    #graph = kneighbors_graph(X=data, n_neighbors=n_neighbors, mode='distance')
+    sc = SpectralClustering(n_clusters=numClust, eigen_solver='arpack', affinity=affinity, n_neighbors=n_neighbors, n_jobs=n_jobs)
+    print 'SpectralClustering...'
+    clustLabels = sc.fit_predict(data)
+    print 'Inverse reshaping...'
+    clustIm = np.reshape(clustLabels, (height, width))
+    print 'Inverse rescaling to %f'% (1.0/(scaleFactor**2))
+    clustIm = np.uint8(rescale(clustIm, 1.0/scaleFactor, preserve_range=True))
+    
+    return clustIm
