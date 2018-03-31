d54e019d102795b785294e1fce33e082542dfcc0
kkkkeeee <zhaikeke1@gmail.com>
2018-03-31 15:17:17 -0400

    added som

--- MyClust05.py
+++ MyClust05.py
@@ -10,6 +10,7 @@ from projectYF import MyGMM05
 from projectYF import MySpectral05
 import MyMartinIndex05 as martin
 from MyFCM05 import MyFCM05
+from MySOM05 import MySOM05
 
 def MyClust05(Im, algStr, alg, imStr, imType, clusStr, numClusts):
     if algStr != "Algorithm":
@@ -21,8 +22,8 @@ def MyClust05(Im, algStr, alg, imStr, imType, clusStr, numClusts):
 
     if alg.lower() == "kmeans" and imType.lower() == "rgb":
         [ClusterIm, CCIm] = MyKmeans05(Im, imType, numClusts)
-    #if alg.lower() == "som" and imType.lower() == "rgb": 
-        #[ClusterIm, CCIm] = kmeans.MyKmeans5(Im, imType, numClusts)   
+    if alg.lower() == "som" and imType.lower() == "rgb": 
+        [ClusterIm, CCIm] = MySOM05(Im, imType, numClusts)   
     if alg.lower() == "fcm" and imType.lower() == "rgb":
         [ClusterIm, CCIm] = MyFCM05(Im, imType, numClusts)
     if alg.lower() == "spectral" and imType.lower() == "rgb":
