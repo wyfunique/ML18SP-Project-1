915b81634f03bd265c3c6083e9e5daea7e761af7
wyfunique <yifanwang2017@gmail.com>
2018-03-31 14:38:54 -0400

    Fixed hyper image bug.

--- projectYF.py
+++ projectYF.py
@@ -30,7 +30,7 @@ def clustIndexShift(clustIm):
 def validateNumClust(numClust):
     assert numClust < 256, 'Error: The number of clusters must be less than 256'
     
-def MyGMM(image, numClust, connectNeighbors, imageType='RGB', filterType=None): # Only support RGB now.
+def MyGMM(image, numClust, connectNeighbors=4, imageType='RGB', filterType=None): 
     # filterType: 'mean', 'median', or None
     # connectNeighbors: 4 or 8, means 4-connectivity or 8-connectivity
     # imageType: 'RGB' or 'Hyper'
@@ -44,23 +44,26 @@ def MyGMM(image, numClust, connectNeighbors, imageType='RGB', filterType=None): 
     n_features = image.shape[2]
     n_samples = height * width
     
-    if imageType == 'Hyper':
+    if imageType == 'Hyper': # Normalized image ranging [0, 1]
         pca = PCA(3)
+        print 'Reshaping image...'
         dataTmp = np.reshape(image, (n_samples, n_features))
-        dataPCA = pca.fit_transform(dataTmp)
-        image = np.uint8(np.reshape(dataPCA, (height, width, 3)))
-    
-    if filterType == 'mean':
-        image = np.uint8(meanRGB(image, kernel4Orig))
-    if filterType == 'median':
-        image = np.uint8(medianRGB(image, kernel4Orig))
-    
-    print 'Reshaping image...'
-    data = np.reshape(image, (n_samples, n_features))
+        data = pca.fit_transform(dataTmp)
+        #image = np.reshape(dataPCA, (height, width, 3))
+        
+    if imageType == 'RGB': # Unnormalized image ranging [0, 255]
+        if filterType == 'mean':
+            image = np.uint8(meanRGB(image, kernel4Orig))
+        if filterType == 'median':
+            image = np.uint8(medianRGB(image, kernel4Orig))
+     
+        print 'Reshaping image...'
+        data = np.reshape(image, (n_samples, n_features))
+        
     print 'Fitting GMM...'
     gmm.fit(data)
     print 'GMM predicting...'
-    clustLabels = gmm.predict(data)
+    clustLabels = gmm.predict(data)        
     print 'Inverse reshaping...'
     clustIm = np.uint8(np.reshape(clustLabels, (height, width)))
     ccIm = np.uint8(label(clustIm, neighbors=connectNeighbors))
@@ -68,7 +71,7 @@ def MyGMM(image, numClust, connectNeighbors, imageType='RGB', filterType=None): 
     ccImOneBased = clustIndexShift(ccIm)
     return clustImOneBased, ccImOneBased
 
-def MySpectral(image, numClust, connectNeighbors, imageType='RGB', scaleFactor=0.3, affinity='nearest_neighbors', n_neighbors=20, n_jobs=-1):
+def MySpectral(image, numClust, connectNeighbors=4, imageType='RGB', scaleFactor=0.3, affinity='nearest_neighbors', n_neighbors=20, n_jobs=-1):
     # imageType: 'RGB' or 'Hyper'
     validateNumClust(numClust)
     """
@@ -83,26 +86,31 @@ def MySpectral(image, numClust, connectNeighbors, imageType='RGB', scaleFactor=0
     width = image.shape[1]
     n_features = image.shape[2]
     n_samples = height * width
-    
-    if imageType == 'Hyper':
+    origShape = image.shape[:2]
+        
+    if imageType == 'Hyper':# Normalized image ranging [0, 1]
+        print 'Rescaling image to %f...'% (scaleFactor**2)
+        image = rescale(image, scaleFactor, preserve_range=True)
+        print 'New shape: %s'%str(image.shape)
         pca = PCA(3)
         dataTmp = np.reshape(image, (n_samples, n_features))
-        dataPCA = pca.fit_transform(dataTmp)
-        image = np.uint8(np.reshape(dataPCA, (height, width, 3)))
-    
-    origShape = image.shape[:2]
-    print 'Rescaling image to %f...'% (scaleFactor**2)
-    image = np.uint8(rescale(image, scaleFactor, preserve_range=True))
-    print 'New shape: %s'%str(image.shape)
-    
-    print 'Reshaping image...'
-    data = np.reshape(image, (n_samples, n_features))
-    #graph = kneighbors_graph(X=data, n_neighbors=n_neighbors, mode='distance')
+        data = pca.fit_transform(dataTmp)
+        #image = np.reshape(dataPCA, (height, width, 3))
+        
+    if imageType == 'RGB': # Unnormalized image ranging [0, 255]
+        print 'Rescaling image to %f...'% (scaleFactor**2)
+        image = np.uint8(rescale(image, scaleFactor, preserve_range=True))
+        print 'New shape: %s'%str(image.shape)
+        print 'Reshaping image...'
+        data = np.reshape(image, (n_samples, n_features))
+        #graph = kneighbors_graph(X=data, n_neighbors=n_neighbors, mode='distance')
+        
     sc = SpectralClustering(n_clusters=numClust, eigen_solver='arpack', affinity=affinity, n_neighbors=n_neighbors, n_jobs=n_jobs)
     print 'SpectralClustering...'
     clustLabels = sc.fit_predict(data)
+        
     print 'Inverse reshaping...'
-    clustIm = np.reshape(clustLabels, (height, width))
+    clustIm = np.uint8(np.reshape(clustLabels, (height, width)))
     print 'Inverse rescaling to %f'% (1.0/(scaleFactor**2))
     clustIm = np.uint8(resize(clustIm, output_shape=origShape, preserve_range=True))
     ccIm = np.uint8(label(clustIm, neighbors=connectNeighbors))
