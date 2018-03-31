f0bcf5e1f9a6e12b4226cdb5b97e6b7ccc54cccc
kkkkeeee <zhaikeke1@gmail.com>
2018-03-31 15:03:10 -0400

    changed function name

--- MyClust05.py
+++ MyClust05.py
@@ -5,10 +5,11 @@ from DisplayImAngSegs import DisplayImAndSegs
 from os import listdir
 from os.path import isfile, join
 import DisplayImAngSegs as Display
-import kmeans
-import projectYF as yf
+from MyKmeans05 import MyKmeans05
+from projectYF import MyGMM05
+from projectYF import MySpectral05
 import MyMartinIndex05 as martin
-import fcm
+from MyFCM05 import MyFCM05
 
 def MyClust05(Im, algStr, alg, imStr, imType, clusStr, numClusts):
     if algStr != "Algorithm":
@@ -19,15 +20,15 @@ def MyClust05(Im, algStr, alg, imStr, imType, clusStr, numClusts):
         print "Fifth parameter should be NumClusts"
 
     if alg.lower() == "kmeans" and imType.lower() == "rgb":
-        [ClusterIm, CCIm] = kmeans.MyKmeans5(Im, imType, numClusts)
+        [ClusterIm, CCIm] = MyKmeans05(Im, imType, numClusts)
     #if alg.lower() == "som" and imType.lower() == "rgb": 
         #[ClusterIm, CCIm] = kmeans.MyKmeans5(Im, imType, numClusts)   
     if alg.lower() == "fcm" and imType.lower() == "rgb":
-        [ClusterIm, CCIm] = fcm.MyFCM(Im, imType, numClusts)
+        [ClusterIm, CCIm] = MyFCM05(Im, imType, numClusts)
     if alg.lower() == "spectral" and imType.lower() == "rgb":
-        [ClusterIm, CCIm] = yf.MySpectral(Im, numClusts, 4)
+        [ClusterIm, CCIm] = MySpectral05(Im, numClusts, 4)
     if alg.lower() == "gmm" and imType.lower() == "rgb":
-        [ClusterIm, CCIm] = yf.MyGMM(Im, numClusts, 4)
+        [ClusterIm, CCIm] = MyGMM05(Im, numClusts, 4)
 
     ## TODO: not implement hyper image yet
     return [ClusterIm, CCIm]
